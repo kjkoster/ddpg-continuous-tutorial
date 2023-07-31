bffa199a0a84a9184906f5232c382d1b0946ceeb
Kees Jan Koster <kjkoster@kjkoster.org>
2023-07-31 12:10:16 +0200

    Add bipedal walker training.

---
+++ torch_walker.py
@@ -0,0 +1,58 @@
+import gym
+import time
+import torch as T
+import numpy as np
+from ddpg_torch import Agent
+import matplotlib.pyplot as plt
+
+print(f"training on device {'cuda:0' if T.cuda.is_available() else 'cpu'}")
+
+def plotLearning(scores, filename, x=None, window=5):   
+    N = len(scores)
+    running_avg = np.empty(N)
+    for t in range(N):
+	    running_avg[t] = np.mean(scores[max(0, t-window):(t+1)])
+    if x is None:
+        x = [i for i in range(N)]
+    plt.ylabel('Score')       
+    plt.xlabel('Game')                     
+    plt.plot(x, running_avg)
+    plt.savefig(filename)
+
+game='BipedalWalker-v3'
+env = gym.make(game)
+
+agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[24], tau=0.001, env=env,
+              batch_size=64, layer1_size=400, layer2_size=300, n_actions=4)
+
+# try loading checkpoint files, if they exist, so we can continue learning on
+# those. If no files exist, we just start fresh.
+try:
+    agent.load_models(f"checkpoints/{game}")
+except FileNotFoundError:
+    pass
+
+score_history = []
+for episode in range(30000):
+    start = time.time()
+    done = False
+    truncated = False
+    score = 0
+    iterations = 0
+    observation, info = env.reset()
+
+    while not (done or truncated):
+        action = agent.choose_action(observation)
+        new_state, reward, done, truncated, info = env.step(action)
+        agent.remember(observation, action, reward, new_state, int(done or truncated))
+        agent.learn()
+        score += reward
+        iterations += 1
+        observation = new_state
+
+    score_history.append(score)
+    print(f"episode {episode}: score {score:.2f}, 100 game average {np.mean(score_history[-100:]):.2f}, took {time.time() - start:.1f} seconds for {iterations} iterations, done {done}, truncated {truncated}")
+    if (episode + 1) % 25 == 0:
+        agent.save_models(f"checkpoints/{game}")
+        plotLearning(score_history, f"{game}.png", window=100)
+
---
+++ visual_torch_walker.py
@@ -0,0 +1,32 @@
+import gym
+import time
+import numpy as np
+from ddpg_torch import Agent
+
+game='BipedalWalker-v3'
+env = gym.make(game, render_mode="human")
+
+agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[24], tau=0.001, env=env,
+              batch_size=64, layer1_size=400, layer2_size=300, n_actions=4)
+agent.load_models(f"checkpoints/{game}")
+
+score_history = []
+for episode in range(10000):
+    start = time.time()
+    done = False
+    truncated = False
+    score = 0
+    iterations = 0
+    observation, info = env.reset()
+
+    while not (done or truncated):
+        action = agent.choose_action(observation)
+        observation, reward, done, truncated, info = env.step(action)
+        score += reward
+        iterations += 1
+        env.render()
+
+    score_history.append(score)
+    delta_t = time.time() - start
+    print(f"episode {episode}: score {score:.2f}, 100 game average {np.mean(score_history[-100:]):.2f}, took {delta_t:.1f} seconds for {iterations} iterations, {iterations/delta_t:.1f} iterations per second, done {done}, truncated {truncated}")
+
